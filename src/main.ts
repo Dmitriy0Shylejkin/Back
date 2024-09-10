43930e601c0bdddca0972b0e44c19f7c45655a9d
Dmitrij <d.shulejkin@nv.dunice.net>
2024-09-10 13:31:29 +0300

    fix 10.09

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 dotenv.config({ path: `.${process.env.NODE_ENV}.env` });
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  // Настройка CORS
+  app.use(
+    cors({
+      origin: 'http://127.0.0.1:5501', // Замените на ваш фронтенд URL
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('NestJS')
